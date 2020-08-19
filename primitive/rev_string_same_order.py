15816c0ca1671299b332d979b6add907b10dee14
islam-shamiul <islamshamiul7@gmail.com>
2020-08-19 13:30:49 +0600

    reverse

---
+++ primitive/rev_string_same_order.py
@@ -0,0 +1,17 @@
+a = "this is a sentense"
+
+x = ""
+y = a.split(" ")
+
+print(len(y))
+print(y[0])
+"""z1 = y[0][::-1]
+z2 = y[1][::-1]
+z3 = y[2][::-1]
+z4 = y[3][::-1]
+print(z1+" "+z2+" "+z3+" "+z4)
+"""
+
+for z in y:
+    x += z[::-1] + " "
+print(x)
---
+++ primitive/reverseString.py
@@ -0,0 +1,5 @@
+x = "this is a sentense"
+
+rev_x = x[::-1]
+
+print(rev_x)
